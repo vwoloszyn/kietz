84a48dcee16309e2b62bc74d66131f87b78fba9e
viniciuswoloszyn <woloszyn@tu-berlin.de>
2020-03-28 16:41:36 +0100

    maps

--- services/django/interface/views.py
+++ services/django/interface/views.py
@@ -23,10 +23,21 @@ import urllib.request
 import json
 import datetime
 from django.shortcuts import redirect
+import urllib.request
+import json
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 
 def index(request):
-    #return render(request,"clean.html",{})
+
+
     return render(request,"index.html",{})
 
 
